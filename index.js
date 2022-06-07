a07ddf8cecaf486cf0614e4ae012d124a15b61cb
Frank-197 <frankomosh197@gmail.com>
2022-06-07 20:34:42 +0300

    Done.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function distanceFromHqInBlocks(blockNumber){
     if(distance <= 400){
       return 0
     } else if (distance > 400 && distance <= 2000){
-      return 2*1600
+      return .02*(distance-400)
     } else if (distance > 2000 && distance < 2500) {
       return 25
     } else {
